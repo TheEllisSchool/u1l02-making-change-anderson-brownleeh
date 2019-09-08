03b2181dca16f52fe4a85e5ce2eba34e6d58849c
hannahanderson-brownlee <hannahanderson-brownlee@172.16.20.247>
2019-09-08 14:24:37 -0400

    Homework

--- src/U1L02NumNickCalc.java
+++ src/U1L02NumNickCalc.java
@@ -1,18 +1,31 @@
 import javax.swing.JOptionPane;
 
 // U1L02 notes-
-//orignailly had problem statement but delated other module
+//orignailly had problem statement but deleted other module
 public class U1L02NumNickCalc {
 
 	public static void main(String[] args) {
 		String input = JOptionPane.showInputDialog(null, "How many cents do you have?");
-		int userMoney = Integer.parseInt(input);
+		int userInput = Integer.parseInt(input);
+		int userMoney = userInput;
 		
 		//System.out.println(userMoney);
-		int numbNicks = userMoney/5;
-		int pennies = userMoney - (numbNicks * 5);
+		//int numbNicks = userMoney/5;
+		//int pennies = userMoney - (numbNicks * 5);
+		int numbQuarters = userMoney/25;
+		userMoney = userMoney % 25;
+		System.out.println(userMoney);
 		
-		JOptionPane.showMessageDialog(null, userMoney + " is "
+		int numbDimes = userMoney/10;
+	    userMoney = userMoney%10;
+		
+		int numbNicks = userMoney/5;
+		userMoney = userMoney %5;
+
+		int pennies = userMoney;
+		
+		JOptionPane.showMessageDialog(null, userInput + " is " + numbQuarters
+		+ " quarters, " + numbDimes + " dimes,  "
 		+ numbNicks + " nickels and " + pennies + " pennies");
 	}
 
